ef007e387a032637a3f738e00444051876c9c76a
Peter Lu <peter.lu723@gmail.com>
2024-06-06 23:37:47 -0700

    updated server.js with default config

--- server/server.js
+++ server/server.js
@@ -1,30 +1,15 @@
-const express = require('express');
-const mongoose = require('mongoose');
+import express from "express";
+import cors from "cors";
+import records from "./routes/record.js";
 
-
+const PORT = process.env.PORT || 5050;
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(express.json()); // For parsing application/json
+app.use(cors());
+app.use(express.json());
+app.use("/record", records);
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
-
-// Define routes
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-// Start server
+// start the Express server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+  console.log(`Server listening on port ${PORT}`);
 });
